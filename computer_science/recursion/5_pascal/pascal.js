266d75fbb00ebed9ec8d65ba24f43f066a64265c
SpyrosPolemis <spy.polemis@gmail.com>
2026-05-22 02:52:32 +1000

    Much easier this time :D

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,16 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) {
+    return [1];
+  }
+  const row = [0, 0];
+  row.splice(1, 0, ...pascal(n - 1));
+  const newRow = [];
+  for (let i = 0; i < row.length - 1; i++) {
+    newRow.push(Number(row[i]) + Number(row[i + 1]));
+  }
+  console.log("returning", newRow);
+  return newRow;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
